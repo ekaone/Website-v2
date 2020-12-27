f65612b99e026ccc8d5e22575cc571fb1848ef7a
ekaone <ekaone3033@gmail.com>
2020-12-27 13:11:28 +0700

    change tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,11 +52,11 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "256538177",
+        trackingId: "G-S5GV45GCQZ",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        // head: false,
+        head: true,
         // // Setting this parameter is optional
-        // anonymize: true,
+        anonymize: true,
         // // Setting this parameter is also optional
         // respectDNT: true,
         // // Avoids sending pageview hits from custom paths
